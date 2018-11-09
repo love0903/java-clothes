d4ba4f3d090cac77ebd8f105d231c72bb142aaec
love0903 <a0958007246@gmail.com>
2018-11-09 21:55:33 +0800

    clothes02

---
+++ src/Clothes/Raircoat.java
@@ -0,0 +1,24 @@
+package Clothes;
+
+public class Raircoat extends Clothes implements artiWaaher {
+
+	public Raircoat() {
+		// TODO 自動產生的建構子 Stub
+	}
+
+	public Raircoat(String size) {
+		super(size);
+		// TODO 自動產生的建構子 Stub
+	}
+
+	public Raircoat(String size, String Color) {
+		super(size, Color);
+		// TODO 自動產生的建構子 Stub
+	}
+
+	public Raircoat(String size, String color, int prize) {
+		super(size, color, prize);
+		// TODO 自動產生的建構子 Stub
+	}
+
+}
---
+++ src/Clothes/artiWaaher.java
@@ -0,0 +1,5 @@
+package Clothes;
+
+public interface artiWaaher {
+	public ab
+}
