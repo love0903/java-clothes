d17e35ad4065e5fe0ae267bb3a22b4049d7d40e0
love0903 <a0958007246@gmail.com>
2018-11-09 20:35:21 +0800

    Clothes02

--- src/Demo.java
+++ src/Demo.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
+import Clothes.*;
+
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 		Pants jeans = new Pants();
+		Clothes clothes = new Clothes();
 		System.out.print("Please input the name of the pants: ");
 		String name = scanner.nextLine();
 	}
---
+++ src/Shoes/shoes.java
@@ -0,0 +1,33 @@
+package Shoes;
+
+public class shoes {
+	
+	private String name;
+	private String color;
+	private int price;
+	public shoes(String name, String color, int price) {
+		this.name = name;
+		this.color = color;
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+
+}
